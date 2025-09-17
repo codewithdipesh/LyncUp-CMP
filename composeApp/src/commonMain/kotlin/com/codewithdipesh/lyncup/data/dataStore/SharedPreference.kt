324e249cb76fc29bd16382d1c26b1cd6dd3da1cb
Dipesh Pal <codewithdipesh2022@gmail.com>
2025-09-17 21:55:10 +0530

    add save connected device in datastore

--- composeApp/src/commonMain/kotlin/com/codewithdipesh/lyncup/data/dataStore/SharedPreference.kt
+++ composeApp/src/commonMain/kotlin/com/codewithdipesh/lyncup/data/dataStore/SharedPreference.kt
@@ -1,18 +1,42 @@
 package com.codewithdipesh.lyncup.data.dataStore
 
+import com.codewithdipesh.lyncup.domain.model.Device
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
 object SharedPreference {
-    private const val KEY = "device_id"
+    private const val ID_KEY = "device_id"
+    private const val IS_CONNECTED_KEY = "isConnected"
+    private const val CONNECTED_DEVICE_KEY = "device_id"
 
     @OptIn(ExperimentalUuidApi::class)
     fun getOrCreateDeviceId() : String {
         val settings = SettingsProvider.settings()
-        val existing = settings.getStringOrNull(KEY)
+        val existing = settings.getStringOrNull(ID_KEY)
         if(existing != null) return existing
         val newId = Uuid.random().toString()
-        settings.putString(KEY,newId)
+        settings.putString(ID_KEY,newId)
         return newId
+    }
+
+    fun isConnected() : Boolean {
+        val settings = SettingsProvider.settings()
+        val existing = settings.getBooleanOrNull(IS_CONNECTED_KEY)
+        if(existing != null) return existing
+        return false
+    }
+
+    fun getConnectedDevice() : Device? {
+        val settings = SettingsProvider.settings()
+        val codedDevice =  settings.getStringOrNull(CONNECTED_DEVICE_KEY)
+        val decoded = Json.decodeFromString<Device>(codedDevice ?: return null)
+        return decoded
+    }
+    fun setConnectedDevice(device: Device) {
+        val settings = SettingsProvider.settings()
+        val codedDevice = Json.encodeToString(device)
+        settings.putString(CONNECTED_DEVICE_KEY,codedDevice)
     }
 }
